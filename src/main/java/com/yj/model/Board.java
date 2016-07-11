a54b63b2c21a646d9d8734b251c5dc5523b4c716
김영재 <young891221@gmail.com>
2016-07-11 18:26:33 +0900

    게시판 작성 폼 추가

---
+++ src/main/java/com/yj/model/Board.java
@@ -0,0 +1,116 @@
+package com.yj.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+@Entity
+@Table(name = "board")
+public class Board {
+	private int id;
+	private String group;
+	private String title;
+	private String image;
+	private String content;
+	private int viewCount;
+	private int status;
+	private DateTime insertDate;
+	private DateTime updateDate;
+	private DateTime deleteDate;
+	private Member editorId;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "board_id")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name = "board_group")
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	@Column(name = "board_title")
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	@Column(name = "board_image")
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	@Column(name = "board_content")
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Column(name = "board_view_count")
+	public int getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	@Column(name = "board_status")
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	@Column(name = "board_insert_date")
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+	public DateTime getInsertDate() {
+		return insertDate;
+	}
+	public void setInsertDate(DateTime insertDate) {
+		this.insertDate = insertDate;
+	}
+	@Column(name = "board_update_date")
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+	public DateTime getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(DateTime updateDate) {
+		this.updateDate = updateDate;
+	}
+	@Column(name = "board_delete_date")
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+	public DateTime getDeleteDate() {
+		return deleteDate;
+	}
+	public void setDeleteDate(DateTime deleteDate) {
+		this.deleteDate = deleteDate;
+	}
+	//@Column(name = "editor_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(nullable = true, name = "editor_id") //name은 자신의 DB칼럼명
+	public Member getEditorId() {
+		return editorId;
+	}
+	public void setEditorId(Member editorId) {
+		this.editorId = editorId;
+	}
+	
+}
